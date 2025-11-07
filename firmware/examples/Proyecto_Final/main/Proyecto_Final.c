aee6680f638713bc3827b59ab83bb4f86a13afb9
Julieta Andreoli <julieta.andreoli@ingenieria.uner.edu.ar>
2025-11-07 12:16:13 -0300

    Funciones de mensajes por UART

--- firmware/examples/Proyecto_Final/main/Proyecto_Final.c
+++ firmware/examples/Proyecto_Final/main/Proyecto_Final.c
@@ -26,6 +26,8 @@
 #include "led.h"
 #include "gpio_mcu.h"
 #include "hc_sr04.h"
+#include "uart_mcu.h"
+
 
 /*==================[macros and definitions]=================================*/
 
@@ -53,6 +55,11 @@ TaskHandle_t mideDistancia_task_handle = NULL;
  */
 TaskHandle_t UART_task_handle = NULL; 
 
+/** @def uint8_t control_OnOff
+ * @brief si es 0 --> apagado; si es 1 --> encendido. Controlado por SWITCH_1 y la lógica de cotrol de derrames.
+ */
+uint8_t control_OnOff=0;
+
 /*==================[internal functions declaration]=========================*/
 
 /** @fn static void controlDeDerrames_task (void)
@@ -69,18 +76,57 @@ static void mideDistancia_task(void);
 
 
 /** @fn static void mideDistancia_Task(void)
-* @brief Tarea que enciende los leds según la distancia medida 
+* @brief Tarea que envía mensajes por UART, los posibles mensajes son: "apagado" 
+(al comienzo del programa, cuando se apaga manualmente o cuando detecta que se retira el recipiente), "cargando...", "fin de carga".
 * @return void
 */
 static void msjUART_task(void);
 
 /*==================[external functions definition]==========================*/
 
+void leer_tecla1(){
+	control_OnOff=!control_OnOff;
+}
+
+
+
+static void msjUART_task(void *pvParameter){
+	while(true){
+		ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+		if(control_OnOff){ /* = 1--> msj de cargando...,*/
+				UartSendString(UART_PC,"Cargando...");
+			if(1){ /*ver si usar otra variable de control o directamente hacer por comparación de la medida*/
+				UartSendString(UART_PC,-"Fin de la carga");
+				control_OnOff=!control_OnOff;
+				vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS); /** cuanto tiempo? */
+			}
+		} else if (!control_OnOff){
+			UartSendString(UART_PC, "Apagado");
+		}
+
+
+
+
+	}
+}
+
+
 
 
 
 void app_main(void){
 
+
+/*Para los mensajes de estado de la UART */
+	SwitchesInit();
+	SwitchActivInt(SWITCH_1, leer_tecla1, NULL); /* para el encendido/apagado */
+	serial_config_t my_uart={
+		.port=UART_PC,
+		.baud_rate = 9600,
+		.func_p= NULL, /* cambiar si decidimos que el encendido se haga por uart */
+		.param_p=NULL	
+	};
+	UartInit(&my_uart);
 
 	
     gpioConf_t gpio5v = {GPIO_19, 1};
